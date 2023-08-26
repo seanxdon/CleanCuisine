41cac4315d4657b28ab963b3a98749b3067d4ab0
Sean McDaniel <seanhb714@gmail.com>
2023-08-26 11:00:19 -0700

    init

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,26 +1,39 @@
-import OpenAI from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
- 
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
 // Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-})
- 
-// IMPORTANT! Set the runtime to edge
-export const runtime = 'edge'
- 
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
-  const { messages } = await req.json()
- 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
+  const { vibe, bio } = await req.json();
+
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages
-  })
+    messages: [
+      {
+        role: 'user',
+        content: `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
+          vibe === 'Funny'
+            ? "Make sure there is a joke in there and it's a little ridiculous."
+            : null
+        }
+          Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
+          bio.slice(-1) === '.' ? '' : '.'
+        }`,
+      },
+    ],
+  });
+
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response)
+  const stream = OpenAIStream(response);
   // Respond with the stream
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,151 @@
-'use client'
- 
-import { useChat } from 'ai/react'
- 
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat()
- 
+'use client';
+
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import DropDown, { VibeType } from '../components/DropDown';
+import Footer from '../components/Footer';
+import Github from '../components/GitHub';
+import Header from '../components/Header';
+import { useChat } from 'ai/react';
+
+export default function Page() {
+  const [bio, setBio] = useState('');
+  const [vibe, setVibe] = useState<VibeType>('Professional');
+  const bioRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollToBios = () => {
+    if (bioRef.current !== null) {
+      bioRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const { input, handleInputChange, handleSubmit, isLoading, messages } =
+    useChat({
+      body: {
+        vibe,
+        bio,
+      },
+      onResponse() {
+        scrollToBios();
+      },
+    });
+
+  const onSubmit = (e: any) => {
+    setBio(input);
+    console.log(bio)
+    handleSubmit(e);
+  };
+
+  const lastMessage = messages[messages.length - 1];
+  const generatedBios = lastMessage?.role === "assistant" ? lastMessage.content : null;
+
   return (
-    <main className="mx-auto w-full h-screen max-w-lg p-24 flex flex-col">
-      <section className="mb-auto m">
-        {messages.map(m => (
-          <div className="mb-4" key={m.id}>
-            {m.role === 'user' ? 'User: ' : 'AI: '}
-            {m.content}
-          </div>
-        ))}
-      </section>
-      <form className="flex space-x-4" onSubmit={handleSubmit}>
-        <input
-          className="rounded-md p-2 text-black"
-          value={input}
-          onChange={handleInputChange}
-          placeholder="Say something..."
-        />
-        <button
-          className="border-solid border-2 border-white p-2 rounded-md"
-          type="submit"
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Header />
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+        <a
+          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
+          href="https://github.com/Nutlope/twitterbio"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Send
-        </button>
-      </form>
-    </main>
-  )
+          <Github />
+          <p>Star on GitHub</p>
+        </a>
+        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
+          Generate your next Twitter bio using chatGPT
+        </h1>
+        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
+        <form className="max-w-xl w-full" onSubmit={onSubmit}>
+          <div className="flex mt-10 items-center space-x-3">
+            <Image
+              src="/1-black.png"
+              width={30}
+              height={30}
+              alt="1 icon"
+              className="mb-5 sm:mb-0"
+            />
+            <p className="text-left font-medium">
+              Copy your current bio{' '}
+              <span className="text-slate-500">
+                (or write a few sentences about yourself)
+              </span>
+              .
+            </p>
+          </div>
+          <textarea
+            value={input}
+            onChange={handleInputChange}
+            rows={4}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={
+              'e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com.'
+            }
+          />
+          <div className="flex mb-5 items-center space-x-3">
+            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
+            <p className="text-left font-medium">Select your vibe.</p>
+          </div>
+          <div className="block">
+            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
+          </div>
+
+          {!isLoading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              type="submit"
+            >
+              Generate your bio &rarr;
+            </button>
+          )}
+          {isLoading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              disabled
+            >
+              <span className="loading">
+                <span style={{ backgroundColor: 'white' }} />
+                <span style={{ backgroundColor: 'white' }} />
+                <span style={{ backgroundColor: 'white' }} />
+              </span>
+            </button>
+          )}
+        </form>
+        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+        <output className="space-y-10 my-10">
+          {generatedBios && (
+            <>
+              <div>
+                <h2
+                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
+                  ref={bioRef}
+                >
+                  Your generated bios
+                </h2>
+              </div>
+              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                {generatedBios
+                  .substring(generatedBios.indexOf('1') + 3)
+                  .split('2.')
+                  .map((generatedBio) => {
+                    return (
+                      <div
+                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                        onClick={() => {
+                          navigator.clipboard.writeText(generatedBio);
+                        }}
+                        key={generatedBio}
+                      >
+                        <p>{generatedBio}</p>
+                      </div>
+                    );
+                  })}
+              </div>
+            </>
+          )}
+        </output>
+      </main>
+      <Footer />
+    </div>
+  );
 }
